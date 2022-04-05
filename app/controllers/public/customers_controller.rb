1e26bc6110452c7ae84e6d107330c233aed09ef2
minooor <minorilax13@gmail.com>
2022-04-05 04:31:18 +0000

    Add the function to public/customers

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,6 +3,16 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to mypage_customers_path
+    else
+      render :edit
+    end
   end
 
   def index
@@ -13,4 +23,11 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :profile_image)
+  end
+
 end
