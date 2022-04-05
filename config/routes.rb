bd2cf84529e7bc3ea18061acd57ca6245bf89935
minooor <minorilax13@gmail.com>
2022-04-05 01:10:53 +0000

    Add registration function to device

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  scope module: :public do
+    root :to => 'homes#top'
+    get 'about' => 'homes#about'
+  
+  end
+  
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions' }
