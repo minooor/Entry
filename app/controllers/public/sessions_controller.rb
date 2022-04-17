1b5028fe45639c91baa0bfa9e92e7139b53e94fa
minooor <minorilax13@gmail.com>
2022-04-17 06:43:43 +0000

    Update simple-calendar layout

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -32,7 +32,10 @@ class Public::SessionsController < Devise::SessionsController
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
       if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+        flash[:notice] = "退会済みです。再度ご登録をしてください。"
         redirect_to new_customer_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
       end
     end
   end
